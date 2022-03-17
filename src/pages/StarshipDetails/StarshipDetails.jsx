cde5381c823a1f6011ff03d7ce8ff92878ff3241
giamazonas <skibinskirenee@gmail.com>
2022-03-17 17:32:00 -0400

    add favicon

--- src/pages/StarshipDetails/StarshipDetails.jsx
+++ src/pages/StarshipDetails/StarshipDetails.jsx
@@ -15,32 +15,34 @@ const StarshipDetails = (starship) => {
   })
   return ( 
     <>
-    <div className="each-ship-container">
-        {starshipDetails.url ?
-        <>
-          <h4 className="each-starship-card">Name: </h4>
-          {starshipDetails.name} 
-          <h4>Model: </h4>
-          {starshipDetails.model}
-          <h4>Manufacturer: </h4>
-          {starshipDetails.manufacturer} 
-          <h4>Max Speed </h4>
-          {starshipDetails.max_atmosphering_speed} 
-          <h4>Crew: </h4>
-          {starshipDetails.crew} 
-          <h4>Passengers: </h4>
-          {starshipDetails.passengers} 
-          <h4>Starship Class: </h4>
-          {starshipDetails.starship_class}
-        </>
-        :
-        <>
-          <h2>Loading Starship Details...</h2>
-        </>
-        }<br /> <br /> <br />
-      <a href="/">
-      <button className="btn">RETURN</button>
-      </a><br />
+    <div className="detail-box">
+      <div className="each-ship-container">
+          {starshipDetails.url ?
+          <>
+            <h4 className="each-starship-card">Name: </h4>
+            {starshipDetails.name} 
+            <h4>Model: </h4>
+            {starshipDetails.model}
+            <h4>Manufacturer: </h4>
+            {starshipDetails.manufacturer} 
+            <h4>Max Speed </h4>
+            {starshipDetails.max_atmosphering_speed} 
+            {/* <h4>Crew: </h4>
+            {starshipDetails.crew} 
+            <h4>Passengers: </h4>
+            {starshipDetails.passengers} 
+            <h4>Starship Class: </h4>
+            {starshipDetails.starship_class} */}
+          </>
+          :
+          <>
+            <h2>Loading Starship Details...</h2>
+          </>
+          }<br /> <br /> <br />
+        <a href="/">
+        <button className="btn">RETURN</button>
+        </a><br />
+      </div>
     </div>
     </>
   );
